ce0650edb5099ca59dfc7f2f577d047870891e8e
Fin Hopkins <fin@draughtleaders.com>
2019-01-28 21:51:35 -0500

    Light animation

--- code.py
+++ code.py
@@ -11,39 +11,72 @@ color = fancy.CRGB(255, 85, 0)
 
 
 class LightStrip:
-  def __init__(self, pixels, x_range, y_range, colors, value = 0):
+  def __init__(self, pixels, x_range, y_range, colors,
+      value = 0,
+      speed = 0.025,
+      t = time.monotonic()):
     self.pixels = pixels
     self.colors = colors if type(colors) is list else [colors]
     self.x_pos = list(x_range)
     self.y_pos = list(y_range)
 
     self.max_val = (self.x_pos[-1] - self.x_pos[0] + 1) * (self.y_pos[-1] - self.y_pos[0] + 1)
-    self.palette_step = 1.0 / self.max_val
+
+    color_count = len(self.colors)
+    palette_linearizer = (color_count - 1) / color_count if color_count > 1 else 1
+    self.palette_step = 1.0 / self.max_val * palette_linearizer
+
+    self.speed = speed
 
     self.value = value
+    self.last_value = value
+    self.last_value_t = t
   
-  def render(self, t):
-    i = 0
+  def set_value(self, value, t = time.monotonic()):
+    self.last_value = self.value
+    self.value = value
+    self.last_value_t = t
+
+  def render(self, t = time.monotonic()):
+    time_delta = int((t - self.last_value_t) / self.speed)
+
+    if self.value > self.last_value:
+      render_value = min(self.value, self.last_value + time_delta)
+    else:
+      render_value = max(self.value, self.last_value - time_delta)
+
+    i = 1
 
     for x in self.x_pos:
       for y in self.y_pos:
-        if i == self.value:
-          break
-
-        c = fancy.palette_lookup(self.colors, i * self.palette_step)
+        if i > render_value:
+          c = fancy.CRGB(0, 0, 0)
+        else:
+          c = fancy.palette_lookup(self.colors, i * self.palette_step)
         self.pixels[x, y] = fancy.gamma_adjust(c, brightness = BRIGHTNESS).pack()
         i = i + 1
 
 
 STRIP_1 = LightStrip(
   pixels = trellis.pixels,
-  x_range = range(4),
+  x_range = range(8),
   y_range = range(1),
-  colors = [fancy.CHSV(1.0), fancy.CHSV(0.5)],
-  value = 4
+  colors = [fancy.CHSV(1.0, 0.7), fancy.CHSV(1.0, 1.0)],
+  value = 8
 )
+
+val = 8
+last_change_t = time.monotonic()
 
 while True:
   t = time.monotonic()
   STRIP_1.render(t)
   trellis.pixels.show()
+
+  if t > last_change_t + 3:
+    if val == 8:
+      val = 0
+    else:
+      val = 8
+    STRIP_1.set_value(val, t=t)
+    last_change_t = t
