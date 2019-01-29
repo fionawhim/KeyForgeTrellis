ba16486ac92cc9de28f8f05e2709fe9b47e2c752
Fin Hopkins <fin@draughtleaders.com>
2019-01-28 22:34:46 -0500

    All 6 keys

--- code.py
+++ code.py
@@ -2,7 +2,7 @@ import time
 import adafruit_trellism4
 import adafruit_fancyled.adafruit_fancyled as fancy
 
-BRIGHTNESS = 0.5
+BRIGHTNESS = 0.3
  
 trellis = adafruit_trellism4.TrellisM4Express()
 trellis.pixels.auto_write = False
@@ -20,7 +20,8 @@ class LightStrip:
     self.x_pos = list(x_range)
     self.y_pos = list(y_range)
 
-    self.max_val = (self.x_pos[-1] - self.x_pos[0] + 1) * (self.y_pos[-1] - self.y_pos[0] + 1)
+    self.max_val = (abs(self.x_pos[-1] - self.x_pos[0]) + 1) * (abs(self.y_pos[-1] - self.y_pos[0]) + 1)
+    print(self.max_val)
     self.max_time = speed * self.max_val
 
     color_count = len(self.colors)
@@ -66,7 +67,15 @@ class LightStrip:
 
 STRIP_1 = LightStrip(
   pixels = trellis.pixels,
-  x_range = range(8),
+  x_range = range(4),
+  y_range = range(1),
+  colors = [fancy.CHSV(1.0, 0.5, 0.5), fancy.CHSV(1.0)],
+  value = 1
+)
+
+STRIP_4 = LightStrip(
+  pixels = trellis.pixels,
+  x_range = range(7, 3, -1),
   y_range = range(1),
   colors = [fancy.CHSV(1.0, 0.5, 0.5), fancy.CHSV(1.0)],
   value = 1
@@ -74,7 +83,15 @@ STRIP_1 = LightStrip(
 
 STRIP_2 = LightStrip(
   pixels = trellis.pixels,
-  x_range = range(8),
+  x_range = range(4),
+  y_range = range(1, 2),
+  colors = [fancy.CHSV(1.0 / 6.0, 0.5, 0.5), fancy.CHSV(1.0 / 6.0)],
+  value = 1
+)
+
+STRIP_5 = LightStrip(
+  pixels = trellis.pixels,
+  x_range = range(7, 3, -1),
   y_range = range(1, 2),
   colors = [fancy.CHSV(1.0 / 6.0, 0.5, 0.5), fancy.CHSV(1.0 / 6.0)],
   value = 1
@@ -82,12 +99,19 @@ STRIP_2 = LightStrip(
 
 STRIP_3 = LightStrip(
   pixels = trellis.pixels,
-  x_range = range(8),
+  x_range = range(4),
   y_range = range(2, 3),
   colors = [fancy.CHSV(0.6, 0.5, 0.5), fancy.CHSV(0.6)],
   value = 1
 )
 
+STRIP_6 = LightStrip(
+  pixels = trellis.pixels,
+  x_range = range(7, 3, -1),
+  y_range = range(2, 3),
+  colors = [fancy.CHSV(0.6, 0.5, 0.5), fancy.CHSV(0.6)],
+  value = 1
+)
 
 last_pressed = set()
 last_pressed_t = 0
@@ -106,19 +130,35 @@ while True:
     for press in pressed - last_pressed:
       if press == (0, 0):
         if STRIP_1.value == 1:
-          STRIP_1.set_value(8, t)
+          STRIP_1.set_value(4, t)
         else:
           STRIP_1.set_value(1, t)
       if press == (0, 1):
         if STRIP_2.value == 1:
-          STRIP_2.set_value(8, t)
+          STRIP_2.set_value(4, t)
         else:
           STRIP_2.set_value(1, t)
       if press == (0, 2):
         if STRIP_3.value == 1:
-          STRIP_3.set_value(8, t)
+          STRIP_3.set_value(4, t)
         else:
           STRIP_3.set_value(1, t)
+      if press == (7, 0):
+        if STRIP_4.value == 1:
+          STRIP_4.set_value(4, t)
+        else:
+          STRIP_4.set_value(1, t)
+      if press == (7, 1):
+        if STRIP_5.value == 1:
+          STRIP_5.set_value(4, t)
+        else:
+          STRIP_5.set_value(1, t)
+      if press == (7, 2):
+        if STRIP_6.value == 1:
+          STRIP_6.set_value(4, t)
+        else:
+          STRIP_6.set_value(1, t)
+
 
 
     last_pressed = pressed
@@ -127,6 +167,9 @@ while True:
   STRIP_1.render(t)
   STRIP_2.render(t)
   STRIP_3.render(t)
+  STRIP_4.render(t)
+  STRIP_5.render(t)
+  STRIP_6.render(t)
   trellis.pixels.show()
 
   fps = fps + 1
